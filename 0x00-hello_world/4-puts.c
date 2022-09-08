1d88dcb19457aa091dea0ed81e27a802333d3ef1
Sani Ibrahim <saniibrahim307@gmail.com>
2022-09-08 09:40:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,6 @@
 */
 int main(void)
 {
-	puts("\"Programming is link building a multilingual puzzle");
-	return (0):
+        puts("\"Programming is link building a multilingual puzzle");
+        return (0);
 }
