e81ab7aecc44d249b6cc323cfbcaa82cb361ebd1
Sani Ibrahim <saniibrahim307@gmail.com>
2022-09-08 10:27:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
 * main - print the string in the put function
 *
 * Description: using the main function
-* this program prints "Programming id like building a multilingual puzzle
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-        puts("\"Programming is link building a multilingual puzzle");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
