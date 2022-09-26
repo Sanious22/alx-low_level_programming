9438420772d8ef68a8a83392d79385ecacfb2baf
Sani Ibrahim <saniibrahim307@gmail.com>
2022-09-26 13:56:15 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,18 @@
-#include "main.h"
-
-/**
- * print_chessboard - prints a given 2d array chess board of chars
- *
- * @a: 2d array containing board
- *
- * Return: void
- */
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
-	}
+#include "main.h"                                                                                                                  
+                                                                                                                                   
+/**                                                                                                                                
+ * print_chessboard - Prints a chessboard.                                                                                         
+ * @a: The chessboard to be printed.                                                                                               
+ */                                                                                                                                
+void print_chessboard(char (*a)[8])                                                                                                
+{                                                                                                                                  
+        int indx1, indx2;                                                                                                          
+                                                                                                                                   
+        for (indx1 = 0; a[indx1][7]; indx1++)                                                                                      
+        {                                                                                                                          
+                for (indx2 = 0; indx2 < 8; indx2++)                                                                                
+                        _putchar(a[indx1][indx2]);                                                                                 
+                                                                                                                                   
+                _putchar('\n');                                                                                                    
+        }                                                                                                                          
 }
