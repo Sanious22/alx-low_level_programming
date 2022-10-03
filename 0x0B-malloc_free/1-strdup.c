f9e055cf7db3d6ef3f7fa3c88be4da96760d1ef2
Sani Ibrahim <saniibrahim307@gmail.com>
2022-10-03 14:42:41 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,41 +1,53 @@
 #include "main.h"
 
+
+
 /**
-*_strdup - returns a pointer to a newly allocated
-*speace in memory, contains a copy
-*string given as parameter.
-*1-@str: string to be copied
-*Return: NULL incase of error
+
+* _strdup -> string duplicator function
+
+* @str: string to be dupliated
+
+* Return: a string pointer
+
 */
 
 char *_strdup(char *str)
-{#include "main.h"
 
-/**
-*_strdup - returns a pointer to a newly allocated
-*speace in memory, contains a copy
-*string given as parameter.
-*1-@str: string to be copied
-*Return: NULL incase of error
-*/
-
-char *_strdup(char *str)
 {
-	char *cpy;
-	int index, len;
-	
+
+	int i = 1, j = 0;
+
+	char *s;
+
+
+
 	if (str == NULL)
+
 		return (NULL);
-	for (index = 0; str[index]; index++)
-		len++;
-	cpy = malloc(sizeof(char) * (len + 1));
-	
-	if (cpy == NULL)
+
+	while (str[i])
+
+		i++;
+
+	s = (char *)malloc(i * sizeof(char) + 1);
+
+	if (s == NULL)
+
 		return (NULL);
-	for (index= 0; str[index]; index++)
+
+	while (j < i)
+
 	{
-		cpy[index] = str[index];
+
+		s[j] = str[j];
+
+		j++;
+
 	}
-	cpy[len] = '\0';
-	return (cpy);
+
+	s[j] = '\0';
+
+	return (s);
+
 }
