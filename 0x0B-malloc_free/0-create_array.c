036d05bc32716cd195ce509be4da5de293fedcd4
Sani Ibrahim <saniibrahim307@gmail.com>
2022-10-04 12:19:30 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,11 +14,9 @@ char *create_array(unsigned int size, char c)
 {
 		char *array;
 		unsigned int index;
-		
 		if (size == 0)
 			return (NULL);
-		array = malloc(sizeof(char) * size);
-		
+		array = malloc(sizeof(char) * size);	
 		if (array == NULL)
 			return (NULL);
 		for (index = 0; index < size; index++)
